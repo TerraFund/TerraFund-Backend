37819b35f62fb80e32255c898632a4091c743436
Ntwali-1 <ntwaliyanis@gmail.com>
2025-11-10 21:57:43 +0200

    refactor: register-api

--- TerraFund/src/main/java/com/example/TerraFund/controllers/AuthController.java
+++ TerraFund/src/main/java/com/example/TerraFund/controllers/AuthController.java
@@ -1,10 +1,18 @@
 package com.example.TerraFund.controllers;
 
+import com.example.TerraFund.dto.RegisterRequest;
+import com.example.TerraFund.services.AuthService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
+    private RegisterRequest registerRequest;
+    private AuthService authService;
 
+    public ResponseEntity<?> register(RegisterRequest registerRequest){
+        return authService.register(registerRequest);
+    }
 }
--- TerraFund/src/main/java/com/example/TerraFund/services/AuthService.java
+++ TerraFund/src/main/java/com/example/TerraFund/services/AuthService.java
@@ -1,11 +1,41 @@
 package com.example.TerraFund.services;
 
+import com.example.TerraFund.dto.RegisterRequest;
+import com.example.TerraFund.entities.User;
+import com.example.TerraFund.repositories.UserRepository;
+import com.example.TerraFund.security.JwtService;
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.stereotype.Service;
 
+@AllArgsConstructor
 @Service
 public class AuthService {
-    public ResponseEntity<?> register(){
-        return null;
+    private UserRepository userRepository;
+    private JwtService jwtService;
+    private final AuthenticationProvider authenticationProvider;
+
+    public ResponseEntity<?> register(RegisterRequest registerRequest){
+        if(!userRepository.existsByEmail("registerRequest.getEmail")){
+            throw new RuntimeException("Email already exists!");
+        }
+
+        try{
+            User user = new User();
+
+            user.setEmail(registerRequest.getEmail());
+            user.setPassword(registerRequest.getPassword());
+            user.setRole(registerRequest.getRole());
+            user.setFirstName(registerRequest.getFirstName());
+            user.setLastName(registerRequest.getLastName());
+            user.setPhoneNumber(registerRequest.getPhoneNumber());
+
+            userRepository.save(user);
+
+            return ResponseEntity.ok().body(jwtService.generateToken(user.getEmail()));
+        }catch (Exception e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 }
