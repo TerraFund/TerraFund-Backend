b03da676e4503f6cbe38cf5e4ccfde2955610d6f
Ntwali-1 <ntwaliyanis@gmail.com>
2025-11-14 09:37:19 +0200

    fix: Land Entity

--- TerraFund/src/main/java/com/example/TerraFund/model/Land.java
+++ TerraFund/src/main/java/com/example/TerraFund/model/Land.java
@@ -3,32 +3,57 @@ package com.example.TerraFund.model;
 import com.example.TerraFund.entities.User;
 import jakarta.persistence.*;
 import lombok.*;
+import java.time.LocalDateTime;
+
+import java.util.List;
 
 @Entity
 @Table(name = "lands")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
+@Builder(toBuilder = true)
 public class Land {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String location;
-    private Double size; // in hectares or acres
-    private String cropSuitability;
-    private String ownershipDocPath;
-    
-    @Builder.Default
-    private boolean published = false;
 
-    private String soilQuality;
-    private String waterSource;
-    private Double elevation;
-    private String region;
-    private String type;
+    @Column(nullable = false)
+    private Double sizeInHectares;
+
+    @Column(nullable = false)
+    private String soilType;
+
+    private String title;
+
+    @Column(length = 2000)
+    private String description;
+
+    private Boolean waterSourceIsAvailable;
+
+    private Boolean roadAccessIsAvailable;
+
+    List<String> demoImages;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+
+    @PrePersist
+    public void onCreate() {
+        createdAt = LocalDateTime.now();
+        updatedAt = createdAt;
+    }
+
+    @PreUpdate
+    public void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
 
     @ManyToOne
     @JoinColumn(name = "owner_id")
