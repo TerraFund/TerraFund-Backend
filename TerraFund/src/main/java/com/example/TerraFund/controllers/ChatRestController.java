fc8e1b76d4f92db1f06133b6ad3fed300eb664f4
Ntwali-1 <ntwaliyanis@gmail.com>
2025-11-24 20:04:56 +0200

    fix chatRestController

--- TerraFund/src/main/java/com/example/TerraFund/controllers/ChatRestController.java
+++ TerraFund/src/main/java/com/example/TerraFund/controllers/ChatRestController.java
@@ -2,6 +2,8 @@ package com.example.TerraFund.controllers;
 
 import com.example.TerraFund.entities.Message;
 import com.example.TerraFund.services.ChatService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +11,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/chat")
+@Tag(name = "6. Chat", description = "Chat-related endpoints")
+@RequestMapping("/api/chat")
 public class ChatRestController {
 
     private final ChatService chatService;
@@ -18,6 +21,7 @@ public class ChatRestController {
         this.chatService = chatService;
     }
 
+    @Operation(summary = "Get chat messages", tags = {"6. Chat"})
     @GetMapping("/messages")
     public ResponseEntity<?> getChatMessages(@RequestParam Long user1, @RequestParam Long user2){
         return chatService.getChatMessages(user1, user2);
