f8539bdc6018141ab355644b97856ff577767b0f
Ntwali-1 <ntwaliyanis@gmail.com>
2025-11-24 20:18:26 +0200

    fix chatRestController

--- TerraFund/src/main/java/com/example/TerraFund/services/ChatService.java
+++ TerraFund/src/main/java/com/example/TerraFund/services/ChatService.java
@@ -2,20 +2,25 @@ package com.example.TerraFund.services;
 
 import com.example.TerraFund.entities.Message;
 import com.example.TerraFund.repositories.MessageRepository;
+import com.example.TerraFund.security.CurrentUser;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class ChatService {
     private final MessageRepository messageRepository;
-
-    public ChatService(MessageRepository messageRepository) {
-        this.messageRepository = messageRepository;
-    }
+    private final CurrentUser currentUser;
 
     public ResponseEntity<?> getChatMessages(Long user1, Long user2) {
+
+        if(!currentUser.get().getId().equals(user1) && !currentUser.get().getId().equals(user2)){
+            return ResponseEntity.badRequest().body("You must be logged in as either user1 or user2 to view chat messages!");
+        }
+
         List<Message> messages =  messageRepository.findConversation(user1, user2);
         return ResponseEntity.ok(messages);
     }
